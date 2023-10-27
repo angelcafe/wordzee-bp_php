936c302415e23f35203049db4f30bbf71ef6ca18
Angel Castro <acastro@csq.es>
2023-10-27 14:35:35 +0200

    Se quitan las plantillas (tpl) para implementar Vue.js Versión sin terminar.

---
+++ front/index-vue.js
@@ -0,0 +1,42 @@
+import {createApp} from 'vue';
+const app = createApp({
+    data() {
+        return {
+            version: '0.4',
+        }
+    },
+    methods: {
+        getPL(punto, linea) {
+            let devolver = {
+                name: `pal${linea}let${punto}`,
+            };
+            if (linea === 6 && punto === 6) {
+                devolver.class = 'btn btn-secondary bg-primary';
+                devolver.value = 'DP';
+            } else if (linea === 7 && punto === 7) {
+                devolver.class = 'btn btn-secondary bg-warning';
+                devolver.value = 'TP';
+            } else {
+                devolver.class = 'btn btn-secondary info me-1';
+            }
+            return devolver;
+        },
+        getRonda(ele, ronda) {
+            if (ele === 'label') {
+                return {
+                    class: 'form-check-label',
+                    for: `ronda${ronda}`,
+                }
+            } else {
+                return {
+                    class: "form-check-input",
+                    id: `ronda${ronda}`,
+                    name: 'ronda',
+                    type: 'radio',
+                    value: `ronda${ronda}`,
+                }
+            }
+        }
+    }
+});
+app.mount('#app');
